758e9a75350883dcb1d0a3269531cdf9bab1a640
danieltabarest <danieltabares17@gmail.com>
2018-03-20 16:44:28 -0500

    add resources android & ios

---
+++ MovieDTT/MovieDTT.Droid/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+using Acr.UserDialogs;
+using Xamarin.Forms.Platform.Android;
+
+namespace MovieDTT.Droid
+{
+	[Activity(Label = "MovieDTT.Droid", Theme = "@style/splashscreen", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+            try
+            {
+                FormsAppCompatActivity.ToolbarResource = Resource.Layout.toolbar;
+                FormsAppCompatActivity.TabLayoutResource = Resource.Layout.tabs;
+
+                base.SetTheme(Resource.Style.MyTheme);
+
+                FFImageLoading.Forms.Droid.CachedImageRenderer.Init();
+
+                base.OnCreate(bundle);
+
+                UserDialogs.Init(this);
+
+                global::Xamarin.Forms.Forms.Init(this, bundle);
+
+                LoadApplication(new App(new AndroidInitializer()));
+            }
+            catch (Exception ex)
+            {
+
+            }
+		}
+	}
+
+	public class AndroidInitializer : IPlatformInitializer
+	{
+		public void RegisterTypes(IUnityContainer container)
+		{
+
+		}
+	}
+}
