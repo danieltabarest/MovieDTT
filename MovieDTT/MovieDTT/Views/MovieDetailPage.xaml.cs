07c46b9c67e0c578ffd7e5795d77d517c9053bc5
danieltabarest <danieltabares17@gmail.com>
2018-03-28 13:52:07 -0500

    add file cs

---
+++ MovieDTT/MovieDTT/Views/MovieDetailPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MovieDTT.Views
+{
+    public partial class MovieDetailPage : ContentPage
+    {
+		public MovieDetailPage()
+		{
+			InitializeComponent();
+		}
+    }
+}
